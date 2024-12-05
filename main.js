79a78d0e3050ffb830abebebc3911121ab30bd24
Esteban11p <164960060+Esteban11p@users.noreply.github.com>
2024-12-04 23:35:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+/* 
+const PRODUCTOS = [
+  { nombre: "Especial", Precio: 15.0 },
+  { nombre: "Triple", Precio: 19.0 },
+  { nombre: "De Pollo", Precio: 13.0 },
+  { nombre: "Super", Precio: 14.0 },
+  { nombre: "Sencilla", Precio: 11.0 },
+];
+*/
+
+/*
+let carrito = [];
+
+function agregarAlCarrito(objeto) {
+  carrito.push(objeto);
+  let carro = document.getElementById("carro");
+
+  const productoNew = document.createElement("div");
+
+  productoNew.innerHTML = `
+    <img src="${objeto.imagen}" alt="${objeto.nombre}">
+    <div>
+      <h3>${objeto.nombre}</h3>
+      <p>Precio: $${objeto.precio.toFixed(2)}</p>
+      <p>Cantidad: ${objeto.cantidad}</p>
+    </div>
+    <button class="boton" onclick="eliminarProducto(this)">Eliminar</button>
+  `;
+
+  carro.appendChild(productoNew);
+
+  return productoNew;
+}
+*/
